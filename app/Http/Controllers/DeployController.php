f07588f1c067617ea0bb361d018532cb14323a80
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-03-04 15:55:28 +0100

    Test to check if pull request is on test branch

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -11,15 +11,19 @@ class DeployController extends Controller
         $githubPayload = $request->getContent();
         $githubHash = $request->header('X-Hub-Signature');
 
-        $localToken = config('app.deploy_secret');
-        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+        $branch = $githubPayload->head->ref;
 
-        if (hash_equals($githubHash, $localHash)) {
-            $root_path = base_path();
-            $process = new Process('cd ' . $root_path . '; ./deploy.sh');
-            $process->run(function ($type, $buffer) {
-                echo $buffer;
-            });
+        if ($branch == 'test') {
+            $localToken = config('app.deploy_secret');
+            $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+    
+            if (hash_equals($githubHash, $localHash)) {
+                $root_path = base_path();
+                $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+                $process->run(function ($type, $buffer) {
+                    echo $buffer;
+                });
+            }
         }
     }
 }
