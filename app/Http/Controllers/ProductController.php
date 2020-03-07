b15e729ec8e1474c59383cc01f7e3f5def3969bc
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-03-07 15:05:26 +0100

    use Storage in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
